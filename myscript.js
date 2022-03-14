18b5bb6115c616c92d3a55b0a64d9f9fb83252eb
emyrue <94935633+emyrue@users.noreply.github.com>
2022-03-14 13:01:49 -0600

    Successfully implement remove button

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 const booksSection = document.querySelector('.books');
-
+let newBooks = [];
 let books = [
   {
     title: 'First Book',
@@ -9,7 +9,19 @@ let books = [
     title: 'Second Book',
     author: 'Second Author',
   },
+  {
+    title: 'Third Book',
+    author: 'Third Author',
+  },
 ];
+
+if (window.localStorage.getItem('bookArray') !== null) {
+  let array = JSON.parse(window.localStorage.getItem('bookArray'));
+  for (let m = 0; m < array.length; m += 1) {
+  newBooks[m] = JSON.parse(window.localStorage.getItem(m.toString()));
+}
+  books = newBooks;
+}
 
 function newObject(bookTitle, bookAuthor) {
   books[books.length] = {
@@ -20,6 +32,11 @@ function newObject(bookTitle, bookAuthor) {
 
 function deleteObject(n) {
   books.splice(n, 1);
+  window.localStorage.setItem('bookArray', JSON.stringify(books));
+  for (let k = 0; k < books.length; k += 1) {
+    window.localStorage.setItem(k.toString(), JSON.stringify(books[k]));
+  }
+  location.reload();
 }
 
 function displayObject(n) {
@@ -35,4 +52,16 @@ for (let i = 0; i < books.length; i += 1) {
   let newElement = document.createElement('section');
   newElement.innerHTML = displayObject(i);
   booksSection.appendChild(newElement);
+}
+
+for (let j = 0; j < books.length; j += 1) {
+  let removeButton = document.querySelectorAll('.remove');
+  removeButton[j].addEventListener('click', function() {deleteObject(j);});
+}
+
+window.localStorage.clear();
+
+window.localStorage.setItem('bookArray', JSON.stringify(books));
+for (let k = 0; k < books.length; k += 1) {
+  window.localStorage.setItem(k.toString(), JSON.stringify(books[k]));
 }
